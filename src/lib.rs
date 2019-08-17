ac3de89702e1eed7b10a6bc0a1a5b6e6b7ddf308
Francesco Cogno <francesco.cogno@outlook.com>
2019-08-17 22:58:04 +0200

    started conversion to futures 0.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,133 +1,143 @@
+#![feature(async_await)]
 extern crate failure;
 extern crate serde_json;
-use futures::future::{done, ok, Either, Future};
-use futures::stream::Stream;
+use futures::compat::Future01CompatExt;
+use futures::future::Future;
+use futures::future::{FutureExt, TryFutureExt};
 use http::StatusCode;
 use hyper::service::service_fn;
 use hyper::Client;
 use hyper::{Body, Request, Response, Server};
-use log::{debug, error, trace};
+use log::{debug, error, info, trace, warn};
 use serde::de::DeserializeOwned;
-use std::fmt::Debug;
 use std::sync::Arc;
 mod render_to_prometheus;
 pub use render_to_prometheus::PrometheusMetric;
 mod metric_type;
 pub use metric_type::MetricType;
+use std::net::SocketAddr;
 
 #[inline]
-fn extract_body(
-    req: hyper::client::ResponseFuture,
-) -> impl Future<Item = String, Error = failure::Error> + Send {
-    req.from_err().and_then(|resp| {
-        debug!("response == {:?}", resp);
-        let (_parts, body) = resp.into_parts();
-        body.concat2()
-            .from_err()
-            .and_then(|complete_body| done(String::from_utf8(complete_body.to_vec())).from_err())
-    })
+async fn extract_body(req: hyper::client::ResponseFuture) -> Result<String, failure::Error> {
+    use futures::compat::Stream01CompatExt;
+    use futures::TryStreamExt;
+
+    let resp = req.compat().await?;
+    debug!("response == {:?}", resp);
+
+    let (_parts, body) = resp.into_parts();
+    let complete_body = body.compat().try_concat().await?;
+
+    let s = String::from_utf8(complete_body.to_vec())?;
+    trace!("extracted text == {}", s);
+
+    Ok(s)
 }
 
-pub fn create_string_future_from_hyper_request(
+pub async fn create_string_future_from_hyper_request(
     request: hyper::Request<hyper::Body>,
-) -> impl Future<Item = String, Error = failure::Error> {
+) -> Result<String, failure::Error> {
     let https = hyper_rustls::HttpsConnector::new(4);
     let client = Client::builder().build::<_, hyper::Body>(https);
 
-    extract_body(client.request(request))
-        .from_err()
-        .and_then(|text: String| {
-            debug!("received_text == {:?}", text);
-            ok(text)
-        })
+    Ok(extract_body(client.request(request)).await?)
 }
 
-pub fn create_deserialize_future_from_hyper_request<T>(
+pub async fn create_deserialize_future_from_hyper_request<T>(
     request: hyper::Request<hyper::Body>,
-) -> impl Future<Item = T, Error = failure::Error>
+) -> Result<T, failure::Error>
 where
     T: DeserializeOwned + std::fmt::Debug,
 {
-    create_string_future_from_hyper_request(request)
-        .and_then(|text| done(serde_json::from_str(&text)).from_err())
-        .and_then(|t: T| {
-            debug!("deserialized object == {:?}", t);
-            ok(t)
-        })
+    let text = create_string_future_from_hyper_request(request).await?;
+    let t = serde_json::from_str(&text)?;
+    debug!("deserialized object == {:?}", t);
+    Ok(t)
 }
 
-fn check_compliance(req: &Request<Body>) -> Result<(), Response<Body>> {
+async fn serve_function<O, F, Fut>(
+    req: Request<Body>,
+    f: F,
+    options: Arc<O>,
+) -> Result<Response<Body>, hyper::Error>
+where
+    F: FnOnce(Request<Body>, Arc<O>) -> Fut,
+    Fut: Future<Output = Result<String, failure::Error>>,
+    O: std::fmt::Debug,
+{
+    trace!(
+        "serve_function:: req.uri() == {}, req.method() == {}",
+        req.uri(),
+        req.method()
+    );
     if req.uri() != "/metrics" {
-        trace!("uri not allowed {}", req.uri());
-        Err(Response::builder()
+        Ok(Response::builder()
             .status(StatusCode::NOT_FOUND)
             .body(hyper::Body::empty())
             .unwrap())
     } else if req.method() != "GET" {
-        trace!("method not allowed {}", req.method());
-        Err(Response::builder()
+        Ok(Response::builder()
             .status(StatusCode::METHOD_NOT_ALLOWED)
             .body(hyper::Body::empty())
             .unwrap())
     } else {
-        Ok(())
+        // everything is ok, let's call the supplied future
+        trace!("serve_function:: options == {:?}", options);
+
+        Ok(match f(req, options).await {
+            Ok(response) => Response::new(Body::from(response)),
+            Err(err) => {
+                warn!("internal server error == {:?}", err);
+
+                Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(Body::from(err.to_string()))
+                    .unwrap()
+            }
+        })
     }
 }
 
-fn handle_request<O, P>(
-    req: Request<Body>,
-    options: Arc<O>,
-    perform_request_box: P,
-) -> impl Future<Item = Response<Body>, Error = failure::Error>
+async fn run_server<O, F, Fut>(addr: SocketAddr, f: F, options: Arc<O>) -> Result<(), hyper::Error>
 where
-    O: Debug + Clone + Send + Sync + 'static,
-    P: FnOnce(
-            Request<Body>,
-            &Arc<O>,
-        ) -> Box<dyn Future<Item = String, Error = failure::Error> + Send + 'static>
-        + Send
-        + Clone
-        + 'static,
+    F: FnOnce(Request<Body>, Arc<O>) -> Fut + Send + Sync + Clone + 'static,
+    Fut: Future<Output = Result<String, failure::Error>> + Send + Sync + 'static,
+    O: std::fmt::Debug + Sync + Send + 'static,
 {
-    trace!("{:?}", req);
+    info!("Listening on http://{}", addr);
 
-    done(check_compliance(&req)).then(move |res| match res {
-        Ok(_) => Either::A(perform_request_box(req, &options).then(|res| match res {
-            Ok(body) => ok(Response::new(Body::from(body))),
-            Err(err) => {
-                error!("internal server error: {:?}", err);
-                ok(Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body(hyper::Body::empty())
-                    .unwrap())
-            }
-        })),
-        Err(err) => Either::B(ok(err)),
-    })
+    let serve_future = Server::bind(&addr).serve(move || {
+        let f = f.clone();
+        let options = options.clone();
+
+        service_fn(move |req| {
+            serve_function(req, f.clone(), options.clone())
+                .boxed()
+                .compat()
+        })
+    });
+
+    serve_future.compat().await
 }
 
-pub fn render_prometheus<O, P>(addr: &::std::net::SocketAddr, options: O, perform_request: P)
+pub fn render_prometheus<O, F, Fut>(addr: SocketAddr, f: F, options: O)
 where
-    O: Debug + Clone + Send + Sync + 'static,
-    P: FnOnce(
-            Request<Body>,
-            &Arc<O>,
-        ) -> Box<dyn Future<Item = String, Error = failure::Error> + Send + 'static>
-        + Send
-        + Clone
-        + 'static,
+    F: FnOnce(Request<Body>, Arc<O>) -> Fut + Send + Sync + Clone + 'static,
+    Fut: Future<Output = Result<String, failure::Error>> + Send + Sync + 'static,
+    O: std::fmt::Debug + Sync + Send + 'static,
 {
-    // let's avoid unnecessary copies of our readonly data
-    let options = Arc::new(options.clone());
+    let o = Arc::new(options);
 
-    let new_svc = move || {
-        let options = options.clone();
-        let perform_request = perform_request.clone();
-        service_fn(move |req| handle_request(req, options.clone(), perform_request.clone()))
-    };
+    let futures_03_future = run_server(addr, f, o);
+    let futures_01_future = futures_03_future
+        .map_err(|err| {
+            error!("{:?}", err);
+            eprintln!("Server failure: {:?}", err)
+        })
+        .boxed()
+        .compat();
 
-    let server = Server::bind(&addr)
-        .serve(new_svc)
-        .map_err(|e| eprintln!("server error: {}", e));
-    hyper::rt::run(server);
+    // Finally, we can run the future to completion using the `run` function
+    // provided by Hyper.
+    hyper::rt::run(futures_01_future);
 }
